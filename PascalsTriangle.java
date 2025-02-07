28e163e6d486fe221e323e50edcac2cbf00c1f23
fowblemason <fowblemason@gmail.com>
2025-02-07 12:53:33 -0800

    Update PascalsTriangle.java

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+// Mason Fowble, Victor Strachan, Jehong Kim, Weihua Huang
+// CS202 Winter 2025
+// Pascal's Triangle Team Assignment 
 
-public class Main {
+public class PascalsTriangle {
     public static void main(String args[]) {
         Scanner myObj = new Scanner(System.in);  
         System.out.println("Welcome to the CS202 Pascal's Triangle project.");
@@ -11,15 +14,15 @@ public class Main {
         System.out.println("The row number is: " + number);
     }
     public static int returnPascalRowNumber(int value) {
-        int toreturn = 1;
+        int toReturn = 1;
         while(true) {
             int nCk = 1;
-            for(int k = 0; k<= toreturn/2; k++) {
+            for(int k = 0; k<= toReturn/2; k++) {
                 if (nCk == value)
-                    return toreturn;
-                nCk = nCk * (toreturn - 1 - k)/(k + 1);
+                    return toReturn;
+                nCk = nCk * (toReturn - 1 - k)/(k + 1);
             }
-            toreturn++;
+            toReturn++;
         }
     }
 }
